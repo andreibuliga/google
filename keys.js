41873b75d8deab81d65a76b8617630630ecefb2b
andreibuliga <andreibuliga.web@gmail.com>
2021-07-18 21:04:25 +0200

    Added all the files

---
+++ keys.js
@@ -0,0 +1,5 @@
+// API KEY - https://developers.google.com//custom-search/v1/using_rest
+export const API_KEY = "AIzaSyD07k0pKC8ANIxyvz6huPa50Gkr378uKBs";
+
+// Context Key - https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = "c4208a298c9ccdab2";
